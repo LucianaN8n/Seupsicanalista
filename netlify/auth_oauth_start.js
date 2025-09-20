8c65c89a50515432c41c87a04250ef3413f930b3
LucianaN8n <lucianadomingosterapeuta@gmail.com>
2025-09-20 12:12:46 -0300

    Delete netlify directory

--- netlify/auth_oauth_start.js
+++
@@ -1,9 +0,0 @@
-// netlify/functions/auth_oauth_start.js
-exports.handler = async (event)=>{
-  const clientId = process.env.GOOGLE_CLIENT_ID || '';
-  const redirect = (process.env.BASE_URL || 'http://localhost:8888') + '/.netlify/functions/auth_oauth_callback';
-  const state = Math.random().toString(36).slice(2);
-  const scope = encodeURIComponent('openid email profile');
-  const url = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${encodeURIComponent(clientId)}&redirect_uri=${encodeURIComponent(redirect)}&scope=${scope}&state=${state}&access_type=online&prompt=select_account`;
-  return { statusCode:302, headers:{ Location: url } };
-};
